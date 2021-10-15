fb8d97896555937ff76ccad6523923bb32b27b95
MathieuElisabeth <mathieuregina75@gmail.com>
2021-10-15 15:01:52 +0200

    (feat) : Add search bar

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const PokemonCard = ({ pokemon }) => {
   console.log(pokemon);
@@ -14,10 +15,25 @@ const PokemonCard = ({ pokemon }) => {
 
 const PokemonList = () => {
   const pokemons = useSelector((state) => state);
+  const [search, setSearch] = useState('')
+  let regex = new RegExp(search,'gi');
+
+  const handleChange = ({target: { value }}) => {
+    setSearch(value)
+  }
   console.log("PokemonLIst", pokemons);
   return (
     <div>
-      {pokemons.map((pokemon) => (
+      <input 
+        className="input-search" 
+        type="text" 
+        value={search} 
+        onChange={handleChange}
+        placeholder="Search pokemon..."
+      />
+      {pokemons
+        .filter(pokemon => regex.test(pokemon.name))
+        .map((pokemon) => (
         <PokemonCard key={pokemon.id} pokemon={pokemon} />
       ))}
     </div>
